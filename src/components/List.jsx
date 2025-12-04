b5128b5c41f243cb445be67f33052682bd712209
IM_SORRY <imwe25@naver.com>
2025-12-04 15:14:23 +0900

    style: 모든 컴포넌트 스타일링 끝

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,15 @@
 import './List.css'
+import TodoItem from './TodoItem'
 
 const List =()=>{
     return (
         <div className="List">
             <h3>Todo List</h3>
             <input placeholder="검색어를 입력하세요"/>
+            <div className="todo_wrappers">
+                <TodoItem/>
+                <TodoItem/>
+            </div>
         </div>
     )
 }
