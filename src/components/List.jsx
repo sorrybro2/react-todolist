92a7b4e1610d579720a417c9188ad8373062a99a
IM_SORRY <imwe25@naver.com>
2025-12-04 14:38:16 +0900

    style: List 컴포넌트 스타일링

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,10 @@
+import './List.css'
+
 const List =()=>{
     return (
-        <div>
-            List
+        <div className="List">
+            <h3>Todo List</h3>
+            <input placeholder="검색어를 입력하세요"/>
         </div>
     )
 }
