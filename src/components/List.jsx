74430f9675ab6b09730af77f9449324b0acc9cbf
IM_SORRY <imwe25@naver.com>
2025-12-05 11:28:26 +0900

    feat: filter와 includes를 활용한 검색 기능 추가

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,36 @@
+import { useState } from 'react'
 import './List.css'
 import TodoItem from './TodoItem'
 
 const List =({todo})=>{
+    const [search, setSearch] = useState("")
+
+    const onChangeSearch =(e)=>{
+        setSearch(e.target.value)
+    }
+
+    const filtering =()=>{
+        if (search === "") {
+            return todo;
+        }else{
+            return todo.filter((td)=>{
+                return td.content.toLowerCase().includes(search.toLowerCase());
+            })
+        }
+    }
+
+    const filterTodo = filtering();
+
     return (
         <div className="List">
             <h3>Todo List</h3>
-            <input placeholder="검색어를 입력하세요"/>
+            <input 
+                value={search} 
+                onChange={onChangeSearch} 
+                placeholder="검색어를 입력하세요"
+            />
             <div className="todo_wrappers">
-                {todo.map((td)=>{
+                {filterTodo.map((td)=>{
                     return (
                         <TodoItem key={td.id} {...td}/>
                     )
